2e6fd0b2ddba8de982ad75095ac78b3c02fa5de8
Harivansh Mathur <mathurharivansh811@gmail.com>
2019-05-30 11:34:52 +0530

    Added display function

--- five_c.cpp
+++ five_c.cpp
@@ -21,3 +21,15 @@ void Matrix::accept()
                 }
         }
 }
+void Matrix::display()
+{
+        for(int i=0; i<3; i++)
+        {
+                cout<<" ";
+                for(int j=0; j<3; j++)
+                {
+                        cout<<a[i][j]<<"\t";
+                }
+                cout<<"\n";
+        }
+}
